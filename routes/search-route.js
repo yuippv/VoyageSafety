c2af484a6e841a5153682b1e300497daf678980c
Gla <watchrapong.gla@mail.kmutt.ac.th>
2021-04-21 13:42:29 +0700

    [เพิ่ม] search

--- routes/search-route.js
+++ routes/search-route.js
@@ -3,37 +3,11 @@ var app = express();
 var router = express.Router();
 var db = require('../database');
 
-//Return home page when root('/') is requsted
-// router.get('/dashboard', function(request, response, next) {
-//     response.render('search');
-// });
-
-// app.get('/search', function(req, res){ //GET method to access DB and return results in JSON
-//   db.query(`SELECT "Email" FROM "Voyage_Safety"."User" WHERE "Email" Like '%`+Email+`%'`, function(err,res){
-//     if(err) throw err;
-//     var data = [];
-//     for(i=0;i<rows.length;i++){
-//       data.push(rows[i].product);
-//     }
-//     res.end(JSON.stringify(data));
-//   });
-// });
 
 router.post('/search', function(request, response){ //POST method to access DB and return results in JSON
     var Email = request.body.input;
     db.query(`SELECT "Email" FROM "Voyage_Safety"."User" WHERE "Email" Like '%`+Email+`%'`, function(err,res){
-        console.log(res.rows.length);
-      //   if(err) throw err;
-      
-      // var data = [];
-      // for(i=0;i<res.rows.length;i++){
-      //   data.push(res.rows[i]);
-      // }
-      console.log(data);
-      response.render('dashboard',{data:res.rows});
-      //res.render('search',{alertMsg:"Your Email Address or password is wrong"});
-    //   res.end(JSON.stringify(data));
-    //  console.log(req.params.input);
+      response.render('dashboard',{email:request.session.Email,data:res.rows});
     });
   });
 
